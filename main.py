bc97bffac750351ff3b464073292c4d03cbe420f
Daisuke Shimamoto <diskshima@gmail.com>
2014-05-13 18:21:50 +0900

    Add SSL support.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import smtplib
 import sys
 
-if len(sys.argv) < 8 :
-  print 'There should be 8 arguments'
+arglen = len(sys.argv)
+
+if arglen < 8 :
+  print 'There should be at least 8 arguments'
   sys.exit(1)
 
 fromaddr = str(sys.argv[1])
@@ -13,9 +15,19 @@ username  = str(sys.argv[5])
 password  = str(sys.argv[6])
 host  = str(sys.argv[7])
 
+ssl = False
+if arglen >= 9 :
+  ssl = sys.argv[8] in [ 'true', '1', 't', 'y', 'yes']
+
 msg = "Subject: " + subject + "\nFrom:" + fromaddr + "\nTo: " + toaddrs + "\n" + body
-server = smtplib.SMTP(host)
-server.starttls()
+
+if ssl == True :
+  server = smtplib.SMTP_SSL(host)
+else :
+  server = smtplib.SMTP(host)
+  server.starttls()
+
 server.login(username,password)
 server.sendmail(fromaddr, toaddrs, msg)
 server.quit()
+
